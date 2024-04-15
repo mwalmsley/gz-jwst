251fad5fd36df6d548c67f91c0c658d232cf264e
Mike Walmsley <walmsleymk1@gmail.com>
2024-04-15 21:25:31 +0200

    tinker

--- zoobot_jwst.sh
+++ zoobot_jwst.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=alldwn_%x.%A_%a.out
-#SBATCH --output=alldwn_%x.%A_%a.out.log
 #SBATCH --time=71:00:00                                # Time limit hrs:min:sec
 #SBATCH --constraint=A100 
 #SBATCH --mem=30G
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task=16
 
-srun /share/nas2/walml/miniconda3/envs/zoobot39_cu118_dev/bin/python /share/nas2/walml/repos/zoobot-foundation/foundation/experiments/fin
+nvidia-smi
+
+srun /share/nas2/walml/miniconda3/envs/zoobot39_cu118_dev/bin/python /share/nas2/walml/repos/zoobot_jwst/zoobot_jwst.py
