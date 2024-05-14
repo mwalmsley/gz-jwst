4f6ad3267edc4ba9a377c056038a3b11e5542d54
AstroORyan <d.oryan@lancaster.ac.uk>
2024-05-14 09:46:26 +0200

    Input the script for creating a 1-band cutout.

--- gzjwst/cutouts_from_catalog.py
+++ gzjwst/cutouts_from_catalog.py
@@ -4,9 +4,36 @@ i.e.: a path to a (in general, multiple) fits files, plus coordinate and sizing 
 Should also expect parameters controlling the cutout colouring, dynamic range, etc
 """
 
+import pandas as pd
+import numpy as np
 
-def make_single_band_cutout(galaxy: pd.Series, todo_more_params):
-    pass  # should return nd array ready to be saved as jpg/png
+from astropy.io import fits
+from astropy.wcs import WCS
+from astropy.nddata import Cutout2D
+from astropy.coordinates import SkyCoord
+import astropy.units as u
+
+
+def make_single_band_cutout(galaxy: pd.Series, data: np.array, header: fits.header.Header):
+    # Check that the FITS file has been provided.
+    if not data or not header:
+        Exception('Path to a FITS file is required!')
+
+    # Note, may have to update the entries extracted here depending on name in final file.
+    ra = galaxy.RA
+    dec = galaxy.DEC
+    size = galaxy.size
+
+    # Create the WCS and a list of SkyCoords.
+    w = WCS(header)
+    coord = SkyCoord(ra = ra * u.deg, dec = dec * u.deg, frame = 'icrs')
+
+    # Create the Cutout
+    cutout = Cutout2D(data, coord, size, wcs = w, mode = 'partial')
+
+    flux_values = cutout.data
+
+    return flux_values    
 
 
 # TODO etc for other band combinations, copy from Hayley's notebook
