a72500c15c8a8b025b667a3aacb6b42be010a3bd
Mike Walmsley <walmsleymk1@gmail.com>
2024-07-09 16:12:55 -0400

    separate out making lists (slow) and running downloads (also slow)

--- scripts/cosmos/download_cosmos.py
+++ scripts/cosmos/download_cosmos.py
@@ -1,15 +1,12 @@
 import logging
 
+import pandas as pd
+
 from gzjwst.survey_products import get_observations, download_mast_products_simplified
 from astroquery.mast import Observations
 
-if __name__ == '__main__':
 
-    logging.basicConfig(level=logging.INFO)
-
-    PROPOSAL_ID = '1727'
-    DATA_DIRECTORY = 'data/cosmos/stage_3_science_products'
-
+def make_cosmos_product_list():
     # get all data products
     # for cosmos, this takes about 5 mins, not instant like the others.
     df = get_observations(proposal_id=PROPOSAL_ID, calib_level=3)
@@ -23,5 +20,18 @@ if __name__ == '__main__':
     df.to_csv(DATA_DIRECTORY + '/products_to_download.csv', index=False)  
     assert not any(df['obsID'].duplicated()), "There are duplicate obsIDs in the COSMOS data"
 
+    return df
+
+if __name__ == '__main__':
+
+    logging.basicConfig(level=logging.INFO)
+
+    PROPOSAL_ID = '1727'
+    DATA_DIRECTORY = 'data/cosmos/stage_3_science_products'
+
+    # df = make_cosmos_product_list()
+    # or for speed
+    df = pd.read_csv(DATA_DIRECTORY + '/products_to_download.csv')
+
     # TODO set max_files=None to download all
-    download_mast_products_simplified(product_uris=df['dataURI'], save_dir=DATA_DIRECTORY, max_files=2, cache=True)  
+    download_mast_products_simplified(product_uris=df['dataURI'], save_dir=DATA_DIRECTORY, max_files=None, cache=True)  
